c20f4a3765ce6d6c16f0feb463b40721bc376abb
pakwai <virtue.sign@gmail.com>
2022-07-02 12:32:46 +0800

    action placeholder for test.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const address: string = core.getInput('address');
+    const network: string = core.getInput('network');
+    core.debug(`processing for: ${network}: ${address}`);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('status', 'success');
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
